effe97810dac8a85a237a8cc9fa2392fe6e45de9
Hijiri Maruo <hjroath1993@gmail.com>
2021-01-15 16:59:58 -0800

    Assignment4 commit

--- pythonclubapp/models.py
+++ pythonclubapp/models.py
@@ -5,8 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class MeetingMinute(models.Model):
-    typename=models.CharField(max_length=255)
-    typedescription=models.TextField(null=True, blank=True)
+    minutesid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    attendance=models.CharField(max_length=255)
+    minutestext=models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.typename
@@ -15,33 +16,32 @@ class MeetingMinute(models.Model):
         db_table='meetingminute'
 
 class Meeting(models.Model):
-    meetingname=models.CharField(max_length=255)
-    productlocation=models.CharField(max_length=255)
+    meetingtitle=models.CharField(max_length=255)
+    meetinglocation=models.CharField(max_length=255)
     meetingtype=models.ForeignKey(MeetingMinute, on_delete=models.DO_NOTHING)
-    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    producttime=models.TimeField()
+    meetingid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    meetingtime=models.TimeField()
     dateentered=models.DateField()
-    price=models.DecimalField(max_digits=6, decimal_places=2)
-    meetingurl=models.URLField()
-    description=models.TextField()
 
     def __str__(self):
-        return self.meetingname
+        return self.meetingtitle
 
     class Meta:
         db_table='meeting'
 
 class Resource(models.Model):
-    title=models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
     location=models.CharField(max_length=255)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     meeting=models.ForeignKey(Meeting, on_delete=models.CASCADE)
     resourcetime=models.TimeField()
     resourcedate=models.DateField()
-    resourcetext=models.TextField()                    
+    resourcetext=models.TextField()
+    resourceurl=models.URLField()
+    description=models.TextField()                   
 
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
         db_table='resource' 
