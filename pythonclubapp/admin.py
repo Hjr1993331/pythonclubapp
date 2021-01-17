7edce847025b3d652cba29f70ccea93abca65426
Hijiri Maruo <hjroath1993@gmail.com>
2021-01-16 17:43:19 -0800

    commit

--- pythonclubapp/admin.py
+++ pythonclubapp/admin.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
-from .models import MeetingMinute, Meeting, Resource
-# Register your models here.
-admin.site.register(MeetingMinute)
-admin.site.register(Meeting)
-admin.site.register(Resource)
-
-
-
-
 # from django.contrib import admin
-# from .models import TechType, Product, Review
+# from .models import MeetingMinute, Meeting, Resource
 # # Register your models here.
-# admin.site.register(TechType)
-# admin.site.register(Product)
-# admin.site.register(Review)
+# admin.site.register(MeetingMinute)
+# admin.site.register(Meeting)
+# admin.site.register(Resource)
+
+
+
+
+from django.contrib import admin
+from .models import TechType, Product, Review
+# Register your models here.
+admin.site.register(TechType)
+admin.site.register(Product)
+admin.site.register(Review)
